c3b16ff3827343c2ec78a01c63043045a694beb9
Juliana <juliana6k@mail.ru>
2023-11-04 02:25:13 +0300

    HW2.1

--- src/channel.py
+++ src/channel.py
@@ -9,6 +9,12 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
         self.youtube = build('youtube', 'v3', developerKey="AIzaSyDWRH3IC7hklbhzPbR61vTpjwp9D8zVAiw")
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = f'https://www.youtube.com/channel/{self.channel_info["items"][0]["id"]}'
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
