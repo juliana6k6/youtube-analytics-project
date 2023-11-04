0b7014d02f6c44cfb5530a501d977643b1fe3378
Juliana <juliana6k@mail.ru>
2023-11-04 23:10:33 +0300

    HW2.1

--- src/channel.py
+++ src/channel.py
@@ -9,14 +9,39 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
         self.youtube = build('youtube', 'v3', developerKey="AIzaSyDWRH3IC7hklbhzPbR61vTpjwp9D8zVAiw")
-        self.title = self.channel['items'][0]['snippet']['title']
-        self.video_count = self.channel['items'][0]['statistics']['videoCount']
-        self.description = self.channel['items'][0]['snippet']['description']
-        self.url = f'https://www.youtube.com/channel/{self.channel_info["items"][0]["id"]}'
-        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
-        self.view_count = self.channel['items'][0]['statistics']['viewCount']
+        self.title = self.youtube['items'][0]['snippet']['title']
+        self.video_count = self.youtube['items'][0]['statistics']['videoCount']
+        self.description = self.youtube['items'][0]['snippet']['description']
+        self.url = f'https://www.youtube.com/channel/{self.channel_id}'
+        self.subscriber_count = self.youtube['items'][0]['statistics']['subscriberCount']
+        self.view_count = self.youtube['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         info = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(json.dumps(info, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        """
+        возвращает объект для работы с YouTube API
+        """
+        youtube = build('youtube', 'v3', developerKey="AIzaSyDWRH3IC7hklbhzPbR61vTpjwp9D8zVAiw")
+        return youtube
+
+    def to_json(self, filename):
+        """
+        сохраняет в файл значения атрибутов экземпляра `Channel`
+        """
+        with open(filename, 'w', encoding='utf-8') as file:
+            information = {
+                "id канала": self.channel_id,
+                "Название канала": self.title,
+                "Описание канала": self.description,
+                "ссылка на канал": self.url,
+                "количество подписчиков": self.subscriber_count,
+                "количество видео": self.video_count,
+                "общее количество просмотров": self.view_count
+            }
+            json.dump(information, file, ensure_ascii=False)
+
