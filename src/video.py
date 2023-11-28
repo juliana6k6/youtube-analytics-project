e2b49c2ef6671b22b2e6860cf49b5fe4c5c14769
Juliana <juliana6k@mail.ru>
2023-11-28 13:00:45 +0300

    HW6

--- src/video.py
+++ src/video.py
@@ -10,12 +10,19 @@ class Video():
     def __init__(self, video_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.video_id = video_id
-        self.youtube = build('youtube', 'v3', developerKey="AIzaSyDWRH3IC7hklbhzPbR61vTpjwp9D8zVAiw")
-        self.video_info = self.youtube.videos().list(id=self.video_id, part="snippet,statistics").execute()
-        self.title = self.video_info['items'][0]['snippet']['title']
-        self.like_count = int(self.video_info['items'][0]['statistics']['likeCount'])
-        self.url = f'https://www.youtube.com/channel/{self.video_id}'
-        self.view_count = int(self.video_info['items'][0]['statistics']['viewCount'])
+        try:
+
+            self.youtube = build('youtube', 'v3', developerKey="AIzaSyDWRH3IC7hklbhzPbR61vTpjwp9D8zVAiw")
+            self.video_info = self.youtube.videos().list(id=self.video_id, part="snippet,statistics").execute()
+            self.title = self.video_info['items'][0]['snippet']['title']
+            self.like_count = int(self.video_info['items'][0]['statistics']['likeCount'])
+            self.url = f'https://www.youtube.com/channel/{self.video_id}'
+            self.view_count = int(self.video_info['items'][0]['statistics']['viewCount'])
+        except IndexError:
+            self.url = None
+            self.title = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return self.title
